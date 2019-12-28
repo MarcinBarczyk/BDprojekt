7ce2d5c5c469e79a26d129355f67a37ac6477d9b
MarcinBarczyk <marcin.barczyk15@gmail.com>
2019-12-28 14:00:47 +0100

    111

--- BD/BD/admin_window.cs
+++ BD/BD/admin_window.cs
@@ -11,11 +11,8 @@ using System.Data.SqlClient;
 
 namespace BD
 {
-    
     public partial class admin_window : Form
     {
-        public static string zgloszenie;
-        public static int id_zg;
         private string connectionString = "Data Source=DESKTOP-CL91JDT\\SQLEXPRESS;Initial Catalog=AdministracjaBudynkami;Integrated Security=True";
         List<string> list = new List<string>();
         public admin_window()
@@ -35,18 +32,8 @@ namespace BD
 
         private void Zgloszenia_Click(object sender, EventArgs e)
         {
-            if(remonty_dla_admina.CheckedItems.Count !=0)
-            {
-                //--------------------otwarcie nowego okna i czyszczenie małych okienek-------------------------//
-                podwykonawca_window okno_podw = new podwykonawca_window();
-                okno_podw.Show();
-                lista_budynkow.Items.Clear();
-                remonty_dla_admina.Items.Clear();
-                realizowane_remonty.Items.Clear();
-                //----------------------------------------------------------------------------------------------//
-            }
-
-
+            podwykonawca_window okno_podw = new podwykonawca_window();
+            okno_podw.Show();
             //po wybraniu z checkboxa zgłoszonych usterek i remontow dodaje je do listy "w realizacji" oraz pokazuje okno gdzie mozna uzupelnic jaka firma bedzie sie tym zajmowac
         }
 
@@ -72,7 +59,6 @@ namespace BD
 
         private void dodawanie_Click(object sender, EventArgs e)
         {
-            //------------------------wybór jakie okno mi się otworzyć-------------------------------//
             if (dodawanie_uzytkownika.Checked)
             {
                 nowy_uzytkownik_window okno_add = new nowy_uzytkownik_window();
@@ -93,7 +79,6 @@ namespace BD
                 nowe_lokale_window okno_lokal = new nowe_lokale_window();
                 okno_lokal.Show();
             }
-            //--------------------------------------------------------------------------------------//
         }
 
         private void dodawanie_uzytkownika_CheckedChanged(object sender, EventArgs e)
@@ -124,77 +109,41 @@ namespace BD
         
 
         private void button1_Click(object sender, EventArgs e)
-        {
-            //----------------------------wypisanie aktualnych numerów budynków do listy--------------------//
-            lista_budynkow.Items.Clear();
+        {   
             String SQL = "SELECT id_budynku FROM budynek";
             SqlConnection con = new SqlConnection(connectionString);
             SqlCommand cmd = new SqlCommand(SQL, con);
             SqlDataReader r = null;
             con.Open();
             r = cmd.ExecuteReader();
+            
             while (r.Read())
             {
-                lista_budynkow.Items.Add(r["id_budynku"]);
+                listBox1.Items.Add(r["id_budynku"]);
            }
-            foreach (var item in lista_budynkow.Items)
+            foreach (var item in listBox1.Items)
             {
                 list.Add(item.ToString());
             }
             con.Close();
-            //----------------------------------------------------------------------------------------------//
         }
 
         private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
         {
-            //-------------------------sprawdzanie wyboru budynku-------------------------------------------//
-            remonty_dla_admina.Items.Clear();
-            realizowane_remonty.Items.Clear();
-            string wartosci;
             int i = 0;
-            string text = lista_budynkow.GetItemText(lista_budynkow.SelectedItem);
-            foreach (var item in lista_budynkow.Items)
+            string text = listBox1.GetItemText(listBox1.SelectedItem); //text jako globalny zeby odczytac dalej
+            foreach (var item in listBox1.Items)
             {
                 if (text == list[i])
                 {
-                    MessageBox.Show("Wybrano budynek " + list[i]);
+                    MessageBox.Show("wybrano " + list[i]);
                 }
                 i++;
             }
-            //----------------------------------------------------------------------------------------------//
-
-            //------------------------załadowanie aktualnych zgłoszeń przeznaczonych dla admina-------------//
-            String SQL = "SELECT zgłoszenie.id_zgłoszenia, zgłoszenie.typ_zgłoszenia, zgłoszenie.id_użytkownika FROM zgłoszenie, budynek WHERE zgłoszenie.id_budynku=budynek.id_budynku AND zgłoszenie.data_realizacji IS NULL AND zgłoszenie.typ_zgłoszenia LIKE 'R%' AND budynek.id_budynku ='"+text+"'AND zgłoszenie.id_zgłoszenia NOT IN(select u2.id_zgłoszenia from usterka u2 where u2.id_zgłoszenia=zgłoszenie.id_zgłoszenia)" ;
-            SqlConnection con = new SqlConnection(connectionString);
-            SqlCommand cmd = new SqlCommand(SQL, con);
-            SqlDataReader r;
-            con.Open();
-            r = cmd.ExecuteReader();
-            while (r.Read())
-            {
-                wartosci = r[1] + " Użytkownik:" + r[2];
-                remonty_dla_admina.Items.Add(wartosci, false);
-                zgloszenie = Convert.ToString(r[1]);
-                id_zg = Convert.ToInt32(r[0]);
-            }
-            con.Close();
-            //----------------------------------------------------------------------------------------------//
-
-            //----------------------------załadowanie aktualnie wykonywanych remontów-----------------------//
-            SQL = "SELECT usterka.typ_usterki, podwykonawca.data_zlecenia FROM zgłoszenie, usterka, podwykonawca  WHERE zgłoszenie.id_budynku='" + text + "' AND zgłoszenie.id_zgłoszenia=usterka.id_zgłoszenia AND  usterka.id_usterki=podwykonawca.id_usterki AND podwykonawca.data_wykonania IS NULL";
-            con = new SqlConnection(connectionString);
-            cmd = new SqlCommand(SQL, con);
-            r = null;
-            con.Open();
-            r = cmd.ExecuteReader();
-            while (r.Read())
-            {
-                wartosci = r[0] + " Data zlecenia :" + r[1];
-                realizowane_remonty.Items.Add(wartosci, false);
-            }
-            con.Close();
-            //----------------------------------------------------------------------------------------------//
         }
     }
 }
 
+
+//nie wiem jak ze zgłaszeniem tego zewnętrznym firmom, trzeba to jeszcze gdzieś jakoś wcisnąć
+// i jeszcze jakieś dodawanie tych meldunkow// jakies miejsce gdzie będzie się wpisywać dane i dodawać użytkownika do bazy zeby mogł sie zalogowac itp.
--- BD/BD/podwykonawca_window.cs
+++ BD/BD/podwykonawca_window.cs
@@ -7,57 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
 
 namespace BD
 {
     public partial class podwykonawca_window : Form
     {
-        private string connectionString = "Data Source=DESKTOP-CL91JDT\\SQLEXPRESS;Initial Catalog=AdministracjaBudynkami;Integrated Security=True";
         public podwykonawca_window()
         {
             InitializeComponent();
-        }
-
-        private void podwykonawca_window_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void do_realizacji_Click(object sender, EventArgs e)
-        {
-
-            //-------------------------załadowanie wybranego zgłoszenia do usterek--------------------------//
-            String SQL = null;
-            SQL = "INSERT INTO usterka (id_zgłoszenia, typ_usterki, data_zlecenia) VALUES (@id_z, @zgloszenie, @data)";
-            SqlConnection con = new SqlConnection(connectionString);
-            con.Open();
-            SqlCommand cmd = new SqlCommand(SQL, con);
-            cmd.Parameters.AddWithValue("@id_z", admin_window.id_zg);
-            cmd.Parameters.AddWithValue("@zgloszenie", admin_window.zgloszenie);
-            cmd.Parameters.AddWithValue("@data", SqlDbType.DateTime).Value = data_zlecenia.Value.Date;
-            cmd.ExecuteScalar();
-            con.Close();
-            //----------------------------------------------------------------------------------------------//
-
-            //--------------------------przekazanie usterki dla firmy odpowiedzialnej-----------------------//
-            string nazwa_firmy = firma_nazwa.Text.ToString();
-            SQL= "INSERT INTO podwykonawca (id_usterki, nazwa_firmy,data_zlecenia) VALUES ((SELECT id_usterki FROM usterka WHERE id_zgłoszenia='"+admin_window.id_zg+"'),@firma,@data) ";
-            con = new SqlConnection(connectionString);
-            con.Open();
-            cmd = new SqlCommand(SQL, con);
-            cmd.Parameters.AddWithValue("@firma",firma_nazwa.Text);
-            cmd.Parameters.AddWithValue("@data", SqlDbType.DateTime).Value = data_zlecenia.Value.Date;
-            cmd.ExecuteScalar();
-            con.Close();
-            //----------------------------------------------------------------------------------------------//
-        }
-
-        private void data_zlecenia_ValueChanged(object sender, EventArgs e)
-        {
-            data_zlecenia.ShowUpDown = true;
-            data_zlecenia.CustomFormat = "YYYY-MM-DD hh:mm:ss[.nnn]";
-            
         }
     }
 }
