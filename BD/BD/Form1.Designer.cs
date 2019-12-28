3196e03a0c1d015b3594d61ed94e06b9f8167377
MarcinBarczyk <marcin.barczyk15@gmail.com>
2019-12-28 14:13:55 +0100

    5

---
+++ BD/BD/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace BD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.textBox_login = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_haslo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(212, 217);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(108, 46);
+            this.LoginButton.TabIndex = 0;
+            this.LoginButton.Text = "Zaloguj";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // textBox_login
+            // 
+            this.textBox_login.Location = new System.Drawing.Point(199, 119);
+            this.textBox_login.Name = "textBox_login";
+            this.textBox_login.Size = new System.Drawing.Size(121, 22);
+            this.textBox_login.TabIndex = 1;
+            this.textBox_login.TextChanged += new System.EventHandler(this.textBox_login_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Hasło";
+            // 
+            // textBox_haslo
+            // 
+            this.textBox_haslo.Location = new System.Drawing.Point(199, 158);
+            this.textBox_haslo.Name = "textBox_haslo";
+            this.textBox_haslo.Size = new System.Drawing.Size(121, 22);
+            this.textBox_haslo.TabIndex = 5;
+            this.textBox_haslo.TextChanged += new System.EventHandler(this.textBox_haslo_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(155, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(223, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Administracja budynków";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 303);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_haslo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_login);
+            this.Controls.Add(this.LoginButton);
+            this.Name = "Form1";
+            this.Text = "Administracja";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.TextBox textBox_login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_haslo;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BD/BD/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BD
+{
+    public partial class Form1 : Form
+    {
+        bool login = false;
+        bool loginAdmin = false;
+        bool loginKons = false;
+        bool password = false;
+        int counterLogin;
+        string pathLoginy = @"C:\Users\asus\source\repos\BDprojekt\BD\BD\loginy.txt";
+        string pathhasla = @"C:\Users\asus\source\repos\BDprojekt\BD\BD\hasla.txt";
+        //string pathLoginy = @"C:\Users\Marcin\source\repos\BDprojekt\BD\BD\loginy.txt";
+        //string pathhasla = @"C:\Users\Marcin\source\repos\BDprojekt\BD\BD\hasla.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void textBox_login_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox_haslo_TextChanged(object sender, EventArgs e)
+        {
+            textBox_haslo.PasswordChar = '*';
+            textBox_haslo.MaxLength = 10;
+            
+        }
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            String loginOkno;
+            String loginPlik;
+            String line;
+            int counter = 0;
+
+            loginOkno = textBox_login.Text;
+
+            System.IO.StreamReader file = new System.IO.StreamReader(pathLoginy);
+            while ((line = file.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+                loginPlik = line;
+                if ((loginOkno == loginPlik) && (loginPlik == "admin"))
+                {
+                    loginAdmin = true;
+                    break;
+                }
+                else if ((loginOkno == loginPlik) && (loginPlik == "konserwator"))
+                {
+                    loginKons = true;
+                    break;
+                }
+                else if (loginOkno == loginPlik)
+                {
+                    login = true;
+                    break;
+                }
+                counter++;
+            }
+            counterLogin = counter;
+            file.Close();
+
+            counter = 0;
+            String PasswordOkno;
+            String PasswordPlik;
+            String linehaslo;
+            PasswordOkno = textBox_haslo.Text.ToString();
+            System.IO.StreamReader filehasla = new System.IO.StreamReader(pathhasla);
+            while ((linehaslo = filehasla.ReadLine()) != null)
+            {
+                if (counter == counterLogin)
+                {
+                    System.Console.WriteLine(linehaslo);
+                    PasswordPlik = linehaslo;
+                    if ((PasswordOkno == PasswordPlik))
+                    {
+                        password = true;
+                    }
+
+                }
+                counter++;
+            }
+            filehasla.Close();
+
+
+            if ((loginAdmin == true) && (password == true))
+            {
+                MessageBox.Show("zalogowany admin");
+                admin_window okno_admin = new admin_window();
+                okno_admin.Show();
+                login = false;
+                loginAdmin = false;
+                loginKons = false;
+                password = false;
+
+            }
+            else if ((loginKons == true) && (password == true))
+            {
+                //trzeba by wychwycić jaki konserwator do pozniejszych zadan//chyba ze przyjmiemy ze jest jeden
+                MessageBox.Show("zalogowany konserwator");
+                konserwator_window okno_kons = new konserwator_window();
+                okno_kons.Show();
+                login = false;
+                loginAdmin = false;
+                loginKons = false;
+                password = false;
+            }
+            else if ((login == true) && (password == true))
+            {
+                MessageBox.Show("zalogowany");
+                user_window okno_user = new user_window();
+                okno_user.Show();
+                login = false;
+                loginAdmin = false;
+                loginKons = false;
+                password = false;
+                //zwykły użytkownik
+                //dodać błędne hasło lub login
+            }
+            
+            else
+            {
+                MessageBox.Show("błędne dane, proszę wpisać ponownie");
+                login = false;
+                loginAdmin = false;
+                loginKons = false;
+                password = false;
+            }
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
