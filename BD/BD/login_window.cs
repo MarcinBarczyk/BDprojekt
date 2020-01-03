a2523147eef55ce314df906c1c75c44a82adc0eb
JestemRudy <hubertszymski@gmail.com>
2020-01-03 16:38:09 +0100

    login_window zakończone

--- BD/BD/login_window.cs
+++ BD/BD/login_window.cs
@@ -12,6 +12,7 @@ using System.Data.SqlClient;
 
 using GlobalVariables;
 using System.Security.Cryptography;
+using System.Threading;
 
 namespace BD
 {
@@ -19,19 +20,11 @@ namespace BD
     public partial class Form1 : Form
     {
         public static int id_user; //do innego okna id użytkownika
-        bool login = false;
-        bool loginAdmin = false;
-        bool loginKons = false;
-        bool password = false;
-        int counterLogin;
 
         SqlConnection con = new SqlConnection();
         SqlCommand com = new SqlCommand();
+        Thread thr;
 
-        //int counterLogin;
-
-        //string pathLoginy = GlobalVar.path_login;
-        //string pathhasla = GlobalVar.path_pass;
         public Form1()
         {
             InitializeComponent();
@@ -43,27 +36,29 @@ namespace BD
         private void textBox_haslo_TextChanged(object sender, EventArgs e)
         {
             textBox_haslo.PasswordChar = '*';
-            textBox_haslo.MaxLength = 10;
+            textBox_haslo.MaxLength = 15;
             
         }
         private void LoginButton_Click(object sender, EventArgs e)
         {
             string my_login = textBox_login.Text;
             string hasło = textBox_haslo.Text;
+            bool logged = false;
 
             con.Open();
             com.Connection = con;
-            com.CommandText = "select hasło from zaloguj where login='" + my_login + "'";
+            com.CommandText = "select z.hasło, u.typ_użytkownika from zaloguj z join użytkownik u on z.id_użytkownika = u.id_użytkownika where z.login='" + my_login + "'";
             using (SqlDataReader rdr = com.ExecuteReader())
             {
                 while (rdr.Read())
                 {
                     string db_hash_pass = rdr[0].ToString();
+                    string db_user_type = rdr[1].ToString();
+
                     //----------------------------hash hasła---------------------------------------//
                     using (SHA256 sha256Hash = SHA256.Create())
                     {
                         byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(hasło));
-
                         StringBuilder builder = new StringBuilder();
                         for (int i = 0; i < bytes.Length; i++)
                         {
@@ -71,121 +66,68 @@ namespace BD
                         }
                         string hash_pass = builder.ToString();
                         //----------------------------------------------------------------------------------------------//
-
-                        if ((my_login == "admin") && (hash_pass == db_hash_pass))
+                        //flagi użytkowników do identyfikacji tożsamości
+                        bool loginAdm = false;
+                        bool loginKon = false;
+                        bool loginUsr = false;
+                        if (db_user_type.Contains("Administrator"))
                         {
-                            MessageBox.Show("zalogowany admin");
+                            loginAdm = true;
+                        }
+                        else if(db_user_type.Contains("Konserwator"))
+                        {
+                            loginKon = true;
+                        }
+                        else
+                        {
+                            loginUsr = true;
+                        }
+                        //sprawdzenie hashy haseł
+                        if ((loginAdm == true) && (hash_pass == db_hash_pass))
+                        {
+                            MessageBox.Show("Zalogowano jako Administrator", "System.Info");
                             admin_window okno_admin = new admin_window();
                             okno_admin.Show();
+                            logged = true;
+                            break;
                         }
-                    }
-                }
-
-
-                /*
-                String loginOkno;
-                String loginPlik;
-                String line;
-                int counter = 0;
-
-                loginOkno = textBox_login.Text;
-
-                System.IO.StreamReader file = new System.IO.StreamReader(pathLoginy);
-                while ((line = file.ReadLine()) != null)
-                {
-                    System.Console.WriteLine(line);
-                    loginPlik = line;
-                    if ((loginOkno == loginPlik) && (loginPlik == "admin"))
-                    {
-                        loginAdmin = true;
-                        break;
-                    }
-                    else if ((loginOkno == loginPlik) && (loginPlik == "konserwator"))
-                    {
-                        loginKons = true;
-                        break;
-                    }
-                    else if (loginOkno == loginPlik)
-                    {
-                        login = true;
-                        break;
-                    }
-                    counter++;
-                }
-                counterLogin = counter;
-                file.Close();
-
-                counter = 0;
-                String PasswordOkno;
-                String PasswordPlik;
-                String linehaslo;
-                PasswordOkno = textBox_haslo.Text.ToString();
-                System.IO.StreamReader filehasla = new System.IO.StreamReader(pathhasla);
-                while ((linehaslo = filehasla.ReadLine()) != null)
-                {
-                    if (counter == counterLogin)
-                    {
-                        System.Console.WriteLine(linehaslo);
-                        PasswordPlik = linehaslo;
-                        if ((PasswordOkno == PasswordPlik))
+                        else if ((loginAdm == true) && (hash_pass != db_hash_pass))
                         {
-                            password = true;
+                            MessageBox.Show("Podano błędne hasło dla użytkownika admin", "Ostrzeżenie", MessageBoxButtons.OK, MessageBoxIcon.Stop);
                         }
-
+                        else if ((loginKon == true) && (hash_pass == db_hash_pass))
+                        {
+                            MessageBox.Show("Zalogowano jako Konserwator", "System.Info");
+                            konserwator_window okno_kons = new konserwator_window();
+                            okno_kons.Show();
+                            logged = true;
+                            break;
+                        }
+                        else if ((loginKon == true) && (hash_pass != db_hash_pass))
+                        {
+                            MessageBox.Show("Podano błędne hasło lub login dla użytkownika", "Ostrzeżenie", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                        }
+                        else if ((loginUsr == true) && (hash_pass == db_hash_pass))
+                        {
+                            MessageBox.Show("Zalogowano jako Użytkownik", "System.Info");
+                            user_window okno_user = new user_window();
+                            okno_user.Show();
+                            logged = true;
+                            break;
+                        }
+                        else if ((loginUsr == true) && (hash_pass != db_hash_pass))
+                        {
+                            MessageBox.Show("Podano błędne hasło lub login dla użytkownika", "Ostrzeżenie", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                        }
+                        else if ((db_hash_pass == " ")||(db_user_type == ""))
+                            MessageBox.Show("Podano błędne dane logowania", "Ostrzeżenie", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                        
                     }
-                    counter++;
                 }
-                filehasla.Close();
-
-
-                if ((loginAdmin == true) && (password == true))
-                {
-                    MessageBox.Show("zalogowany admin");
-                    admin_window okno_admin = new admin_window();
-                    okno_admin.Show();
-                    login = false;
-                    loginAdmin = false;
-                    loginKons = false;
-                    password = false;
-
-                }
-                else if ((loginKons == true) && (password == true))
-                {
-                    //trzeba by wychwycić jaki konserwator do pozniejszych zadan//chyba ze przyjmiemy ze jest jeden
-                    MessageBox.Show("zalogowany konserwator");
-                    konserwator_window okno_kons = new konserwator_window();
-                    okno_kons.Show();
-                    login = false;
-                    loginAdmin = false;
-                    loginKons = false;
-                    password = false;
-                }
-                else if ((login == true) && (password == true))
-                {
-                    MessageBox.Show("zalogowany");
-                    user_window okno_user = new user_window();
-                    okno_user.Show();
-                    login = false;
-                    loginAdmin = false;
-                    loginKons = false;
-                    password = false;
-                    //zwykły użytkownik
-                    //dodać błędne hasło lub login
-                }
-
-                else
-                {
-                    MessageBox.Show("błędne dane, proszę wpisać ponownie");
-                    login = false;
-                    loginAdmin = false;
-                    loginKons = false;
-                    password = false;
-                }
-                */
-
             }
-
             con.Close();
+            if (logged == true)
+                Visible = false;
         }
 
         private void Form1_Load(object sender, EventArgs e)
