4eca9901e5b8465b9fc67b2385e468fb1960153f
JestemRudy <hubertszymski@gmail.com>
2020-01-04 11:24:53 +0100

    nowy_u zmiany - niedokońcczony

--- BD/BD/nowy_uzytkownik_window.cs
+++ BD/BD/nowy_uzytkownik_window.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Data.SqlClient;
+using System.Configuration;
 
 using GlobalVariables;
 using System.Security.Cryptography;
@@ -23,10 +24,9 @@ namespace BD
         SqlConnection con = new SqlConnection();
         SqlCommand com = new SqlCommand();
         SqlCommand com2 = new SqlCommand();
-
-        string pathLoginy = GlobalVar.path_login;
-        string pathhasla = GlobalVar.path_pass;
-
+        SqlCommand com3 = new SqlCommand();
+        SqlDataAdapter ada = new SqlDataAdapter();
+        DataTable dt = new DataTable();
         public nowy_uzytkownik_window()
         {
             InitializeComponent();
@@ -35,6 +35,12 @@ namespace BD
 
         private void add_user_Click(object sender, EventArgs e)
         {
+            //Generowanie loginu użytkownika
+            string cz1 = imię.Text;
+            string cz2 = nazwisko.Text;
+            string cz3 = id_użytkownika.Text;
+            string login_użytkownika = cz1.Substring(0, 4) + cz2.Substring(0, 2) + cz3;
+
             string hasło = hasło_text.Text;
 
             //----------------------------hash hasła---------------------------------------//
@@ -64,13 +70,13 @@ namespace BD
                 {
                     con.Open();
                     com.Connection = con;
-                    com.CommandText = "insert into użytkownik(id_użytkownika, id_najemca, typ_użytkownika, imię, nazwisko) values('" + id_użytkownika.Text + "','" + id_najemcy + "','" + typy_uzytkownika.Text + "','" + imię.Text + "','" + nazwisko.Text + "')";
+                    com.CommandText = "insert into użytkownik(id_użytkownika, id_najemca, typ_użytkownika, imię, nazwisko) values('" + id_użytkownika.Text + "','" + id_najemcy.Text + "','" + typy_uzytkownika.Text + "','" + imię.Text + "','" + nazwisko.Text + "')";
                     com.ExecuteNonQuery();
                     con.Close();
                 }
                 con.Open();
                 com2.Connection = con;
-                com2.CommandText = "insert into zaloguj(id_użytkownika, login, hasło) values('" + id_użytkownika.Text + "','" + login_text.Text + "','" + hasło_hash + "')";
+                com2.CommandText = "insert into zaloguj(id_użytkownika, login, hasło) values('" + id_użytkownika.Text + "','" + login_użytkownika + "','" + hasło_hash + "')";
                 com2.ExecuteNonQuery();
                 con.Close();
                 //----------------------------------------------------------------------------------------------//
@@ -87,8 +93,6 @@ namespace BD
 
         private void nowy_uzytkownik_window_Load(object sender, EventArgs e)
         {
-            // TODO: Ten wiersz kodu wczytuje dane do tabeli 'administracjaBudynkamiDataSet.najemca' . Możesz go przenieść lub usunąć.
-            this.najemcaTableAdapter.Fill(this.administracjaBudynkamiDataSet.najemca);
             // TODO: Ten wiersz kodu wczytuje dane do tabeli 'administracjaBudynkamiDataSet.użytkownik' . Możesz go przenieść lub usunąć.
             this.użytkownikTableAdapter.Fill(this.administracjaBudynkamiDataSet.użytkownik);
 
@@ -96,8 +100,17 @@ namespace BD
             var items = typy_uzytkownika.Items;
             items.Add("Administrator");
             items.Add("Konserwator");
-            items.Add("Sprzątacz");
+            //items.Add("Sprzątacz");
             items.Add("Mieszkaniec");
+
+            con.Open();
+            com3.Connection = con;
+            com3.CommandText = "SELECT u.id_użytkownika, u.id_najemca, u.typ_użytkownika, u.imię, u.nazwisko, z.login FROM dbo.użytkownik u join dbo.zaloguj z on u.id_użytkownika = z.id_użytkownika";
+            com3.ExecuteNonQuery();
+            ada.SelectCommand = com3;
+            ada.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -112,27 +125,23 @@ namespace BD
 
         private void login_text_TextChanged(object sender, EventArgs e)
         {
-            String line;
-            String okno;
-            String login_plik;
-            System.IO.StreamReader file = new System.IO.StreamReader(pathLoginy);
-            okno = login_text.Text;
-            while ((line = file.ReadLine()) != null)
-            {
-                System.Console.WriteLine(line);
-                login_plik = line;
-                if (okno == login_plik)
-                {
-                    MessageBox.Show("Podana nazwa już istanieje");
-                }
-            }
-            file.Close();
+
         }
 
         private void hasło_text_TextChanged(object sender, EventArgs e)
         {
             hasło_text.PasswordChar = '*';
-            hasło_text.MaxLength = 10;
+            hasło_text.MaxLength = 15;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
